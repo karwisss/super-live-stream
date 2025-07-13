7a19ae3cc613dd1a63eef1a74415e17c2a5fc939
karwisss <marcin.galecki.t@gmail.com>
2025-07-14 00:14:21 +0200

    Delete player.js

--- player.js
+++
@@ -1,8 +0,0 @@
-const video = document.getElementById('livePlayer');
-if (Hls.isSupported()) {
-  const hls = new Hls();
-  hls.loadSource('https://yourdomain.com/stream.m3u8');
-  hls.attachMedia(video);
-} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-  video.src = 'https://yourdomain.com/stream.m3u8';
-}
